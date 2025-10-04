eee830d2af19a2f6f014908923669faddd79d5e9
JARVIS3954P <gaintacolyte9@gmail.com>
2025-10-04 23:46:28 +0530

    project setup

---
+++ backend/src/test/java/com/jarvis/RetrieveEx/RetrieveExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvis.RetrieveEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrieveExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
